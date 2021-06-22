3fd5a97300a0eef0638868c74f61f2940da0ec16
Nicholas Konzen <nicholaskonzen@gmail.com>
2021-06-22 16:47:26 -0700

    Added descriptions to projects

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -36,19 +36,16 @@ function Portfolio() {
 
                                 <h2 className='project-title'>Inn at the Edge of Copyright</h2>
 
-                                <p className='project-description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis unde
-                                voluptatem inventore ipsam, nemo deleniti suscipit exercitationem assumenda perspiciatis blanditiis ut
-                                dolorem corporis veniam est
-          consectetur delectus quo autem perferendis dolor enim? Laudantium, vitae exercitationem.</p>
+                                <p className='project-description px-3'>The Inn at the Edge of Copyright is a text-based program for anyone with a crave for an adventure. Our project is a MUD or Multi-User-Dungeon RPG loosely based on The Hitchhiker's Guide to the Galaxy series by Douglas Adams and inspired by text-based MUDs of the early days of the internet. The Inn At The Edge is where all who are lost can find a home.</p>
 
                                 <a className='btn btn-dark' href='https://github.com/n-lambert/Inn-At-The-Edge-of-Copyright' target='_blank' rel="noreferrer">
                                     <i className="fab fa-github"></i>
-          View on GitHub
-        </a>
+                                    View on GitHub
+                                </a>
 
                                 <a className='btn btn-green' target='_blank' rel="noreferrer" href='https://innattheedge.herokuapp.com/'>
                                     View the Deployed Application
-        </a>
+                                </a>
 
                             </div>
 
@@ -62,16 +59,16 @@ function Portfolio() {
 
                                 <h2 className='project-title'>The Fab Five's Fantastic Foods</h2>
 
-                                <p className='project-description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis unde voluptatem inventore ipsam, nemo deleniti suscipit exercitationem assumenda perspiciatis blanditiis ut dolorem corporis veniam est consectetur delectus quo autem perferendis dolor enim? Laudantium, vitae exercitationem.</p>
+                                <p className='project-description px-3'>This is a meal planner that not only helps you keep track of breakfast, lunch, and dinner. But it also let's you search for recipes to add to your schedule, and has an interactive grocery list. We also have a contact page to report any issues or ask for help.</p>
 
                                 <a className='btn btn-dark' target='_blank' rel="noreferrer" href="https://github.com/tbsanders5/mealplanner">
                                     <i className="fab fa-github"></i>
-          View on GitHub
-        </a>
+                                    View on GitHub
+                                </a>
 
                                 <a className='btn btn-green' target='_blank' rel="noreferrer" href='https://tbsanders5.github.io/mealplanner/'>
                                     View the Deployed Application
-        </a>
+                                </a>
 
                             </div>
 
@@ -85,19 +82,16 @@ function Portfolio() {
 
                                 <h2 className='project-title'>Javascript Quiz</h2>
 
-                                <p className='project-description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis unde
-                                voluptatem inventore ipsam, nemo
-                                deleniti suscipit exercitationem assumenda perspiciatis blanditiis ut dolorem corporis veniam est
-          consectetur delectus quo autem perferendis dolor enim? Laudantium, vitae exercitationem.</p>
+                                <p className='project-description px-3'>This javascript quiz was created to test my own javascript skills while at the same time testing my knowledge of javascript trivia. Although I created the quiz for myself, I've made it very easy to change or add questions to the quiz. The quiz works by running you through a series of multiple choice questions while running a countdown from 75 seconds. Your overall score is based on how many seconds you have left once the quiz is completed.</p>
 
                                 <a className='btn btn-dark' target='_blank' rel="noreferrer" href='https://github.com/NTKonzen/Javascript-Quiz'>
                                     <i className="fab fa-github"></i>
-          View on GitHub
-        </a>
+                                    View on GitHub
+                                </a>
 
                                 <a className='btn btn-green' target='_blank' rel="noreferrer" href='https://ntkonzen.github.io/Javascript-Quiz/'>
                                     View the Deployed Application
-        </a>
+                                </a>
 
                             </div>
 
@@ -111,21 +105,18 @@ function Portfolio() {
 
                                 <h2 className='project-title'>CLI Employee Tracker</h2>
 
-                                <p className='project-description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis unde
-                                voluptatem inventore ipsam, nemo
-                                deleniti suscipit exercitationem assumenda perspiciatis blanditiis ut dolorem corporis veniam est
-          consectetur delectus quo autem perferendis dolor enim? Laudantium, vitae exercitationem.</p>
+                                <p className='project-description'>This is a command line interface program that assists the user in keeping track of their employees. I built this project to practice my skills in MySQL and Node.js, but this is an open source project that can be utilized by anyone who needs an easy way to manage their employee's. The packages I used to build this project were mysql, console.table, and inquirer.</p>
 
                                 <a className='btn btn-dark' target='_blank' rel="noreferrer" href='https://github.com/NTKonzen/CLI-Employee-Tracker'>
                                     <i className="fab fa-github"></i>
-          View on GitHub
-        </a>
+                                    View on GitHub
+                                </a>
 
                                 <a className='btn btn-green' target='_blank' rel="noreferrer"
                                     href='https://www.youtube.com/watch?v=P8-wGqVaaqc&feature=youtu.be'>
                                     <i className="fab fa-youtube"></i>
-          Watch the Tutorial
-        </a>
+                                    Watch the Tutorial
+                                </a>
 
                             </div>
 
@@ -139,21 +130,18 @@ function Portfolio() {
 
                                 <h2 className='project-title'>Employee Page Generator</h2>
 
-                                <p className='project-description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis unde
-                                voluptatem inventore ipsam, nemo
-                                deleniti suscipit exercitationem assumenda perspiciatis blanditiis ut dolorem corporis veniam est
-          consectetur delectus quo autem perferendis dolor enim? Laudantium, vitae exercitationem.</p>
+                                <p className='project-description'>This is a simple command line application that generates a simple employee html document for any tech company all based off of user input. This includes three 'types' of employees, those being: managers, engineers, and interns. I wrote this program primarily to test my understanding of javascript classes and subclasses, but this can be used by anyone that needs a quick and easy way to build an employee page for their website! In general, this saves a lot of time as opposed to building the page from scratch. This application was built using javascript and a few Node.js packages.</p>
 
                                 <a className='btn btn-dark' target='_blank' rel="noreferrer" href='https://github.com/NTKonzen/Employee-Page-Generator'>
                                     <i className="fab fa-github"></i>
-          View on GitHub
-        </a>
+                                    View on GitHub
+                                </a>
 
                                 <a className='btn btn-green' target='_blank' rel="noreferrer"
                                     href='https://www.youtube.com/watch?v=Rsm47FpZLDU&feature=youtu.be'>
                                     <i className="fab fa-youtube"></i>
-          Watch the Tutorial
-        </a>
+                                    Watch the Tutorial
+                                </a>
 
                             </div>
 
