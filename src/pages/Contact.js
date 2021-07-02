24970b5376621c682781a6743110eb8ebdaf6200
Nicholas Konzen <nicholaskonzen@gmail.com>
2021-07-01 19:31:12 -0700

    Added phone number formatting on input

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
 import Footer from "../components/Footer/Footer"
 import Header from "../components/Header/Header"
 
 function Contact() {
+
+    const [phone, setPhone] = useState('');
+
+    function handlePhoneInput({ target: { value: v } }) {
+        v = v.replace(/[^\d]/g, '');
+        const currentLength = v.length;
+        let newNumber = '';
+
+        if (v.length) {
+            if (v.length <= 3) {
+                newNumber = `(${v})`
+            } else if (currentLength <= 6) {
+                newNumber = `(${v.slice(0, 3)}) ${v.slice(3)}`
+            } else {
+                newNumber = `(${v.slice(0, 3)}) ${v.slice(3, 6)}-${v.slice(6, 14)}`
+            }
+        }
+        setPhone(newNumber)
+    }
+
+    function handleBackspace({ key, target: { value: v } }) {
+        if (key == "Backspace") {
+            v = v.replace(/[^\d]/g, '');
+            let newNumber = "";
+            if (v.length && v.length <= 3) {
+                newNumber = v.slice(0, v.length - 1)
+                handlePhoneInput({ target: { value: newNumber } })
+            }
+        }
+    }
+
     return (
         <div>
             <Header isContact={true}></Header>
@@ -27,9 +60,9 @@ function Contact() {
                         <label className="form-sub-label" htmlFor="input_7_full" id="sublabel_7_masked"
                             aria-hidden="false"> Please enter a valid phone number. </label>
                         <input type="tel" id="input_7_full" name="q7_phoneNumber[full]" data-type="mask-number"
-                            data-masked="true"
-                            placeholder="(000) 000-0000" data-component="phone"
-                            aria-labelledby="sublabel_7_masked" />
+                            data-masked="true" placeholder="(000) 000-0000"
+                            data-component="phone" aria-labelledby="sublabel_7_masked"
+                            value={phone} onChange={handlePhoneInput} onKeyDown={handleBackspace} />
 
                         <label id="label_6" htmlFor="input_6"> Message </label>
                         <div id="cid_6" data-layout="full">
